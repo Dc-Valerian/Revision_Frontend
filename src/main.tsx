ad47a3ddb4501cbb425d8aeef0cf01df5ec44e8f
Dc Valerian <Dc-Valerian>
2023-05-17 15:56:58 -0700

    added

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,12 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import { RouterProvider } from 'react-router-dom'
+import { Element } from './Routes/routes.tsx'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={Element}/>
+    {/* <App /> */}
   </React.StrictMode>,
 )
