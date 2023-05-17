de6acab0fb30a6f41a9ee38f4936505a5cd260fd
Dc Valerian <Dc-Valerian>
2023-05-17 16:38:31 -0700

    added

--- src/components/Static/Header.tsx
+++ src/components/Static/Header.tsx
@@ -1,11 +1,17 @@
 import React from 'react';
+import img from "../../assets/logo.jpg"
 
 const Header = () => {
   return (
-    <div className='w-screen h-[70px] bg-[rgba(0,0,0,0.9)] flex justify-center items-center'>
-       <div className='w-11/12 bg-slate-600'>
-        <div>
-            
+    <div className='w-screen h-[70px] bg-[rgba(0,0,0,0.3)] flex justify-center items-center'>
+       <div className='w-11/12 flex justify-between'>
+        <img src={img} alt="logo" className='h-14'/>
+
+        <div className="flex items-center  gap-5">
+            <p className='flex items-center' >Home</p>
+            <p  className=''>About</p>
+            <p  className=''>Route</p>
+            <p  className=''>Contact</p>
         </div>
        </div>
     </div>
