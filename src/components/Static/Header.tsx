4f5300009abca82892aacc50d28a56aee954f4d7
Dc Valerian <Dc-Valerian>
2023-05-17 16:14:45 -0700

    added

--- src/components/Static/Header.tsx
+++ src/components/Static/Header.tsx
@@ -2,8 +2,12 @@ import React from 'react';
 
 const Header = () => {
   return (
-    <div>
-        Header
+    <div className='w-screen h-[70px] bg-[rgba(0,0,0,0.9)] flex justify-center items-center'>
+       <div className='w-11/12 bg-slate-600'>
+        <div>
+            
+        </div>
+       </div>
     </div>
   );
 }
